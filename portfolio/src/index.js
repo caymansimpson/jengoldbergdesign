f52d61d8b23cb14cbf97fad2d09feba56e11ac25
Cayman Simpson <cayman@fb.com>
2019-05-13 09:10:32 -0700

    Finished all Routing between home and projects pages with parameter passing. Now need to implement projects page

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const exampleBothProject = {
@@ -13,10 +13,10 @@ const exampleBothProject = {
       {'name': 'Summary', 'description': 'Curabitur fermentum finibus tellus, et efficitur augue dignissim sit amet. Cras a posuere lectus. Nulla posuere consectetur sapien, et accumsan urna condimentum a. Morbi viverra mi lorem, id bibendum neque mattis mollis. Aenean sit amet consequat nibh. Integer turpis felis, euismod in nisi nec, imperdiet faucibus magna. Morbi quam eros, accumsan quis rhoncus eget, mattis vel metus. Duis eu sapien libero. Vestibulum imperdiet rhoncus augue, in eff'}
   ],
   'design': [ // List of sections for Graphic Design
-    {'name': 'Context','description': 'Rendezvous Cafe is a restaurant offering job training for people who are rebuilding their lives. The catering business allows the cafe to reian profitable but had a confusing and inefficient process for requests. Identify user and staff painpoints when processing their requests, simplify, and develop a solution.'},
-    {'name': 'Process','description': 'Curabitur fermentum finibus tellus, et efficitur augue dignissim sit amet. Cras a posuere lectus. Nulla posuere consectetur sapien, et accumsan urna condimentum a. Morbi viverra mi lorem, id bibendum neque mattis mollis. Aenean sit amet consequat nibh. Integer turpis felis, euismod in nisi nec, imperdiet faucibus magna. Morbi quam eros, accumsan quis rhoncus eget, mattis vel metus. Duis eu sapien libero. Vestibulum imperdiet rhoncus augue, in ef'},
-    {'name': 'Summary', 'description': 'Curabitur fermentum finibus tellus, et efficitur augue dignissim sit amet. Cras a posuere lectus. Nulla posuere consectetur sapien, et accumsan urna condimentum a. Morbi viverra mi lorem, id bibendum neque mattis mollis. Aenean sit amet consequat nibh. Integer turpis felis, euismod in nisi nec, imperdiet faucibus magna. Morbi quam eros, accumsan quis rhoncus eget, mattis vel metus. Duis eu sapien libero. Vestibulum imperdiet rhoncus augue, in eff'}
-]
+      {'name': 'Context','description': 'Rendezvous Cafe is a restaurant offering job training for people who are rebuilding their lives. The catering business allows the cafe to reian profitable but had a confusing and inefficient process for requests. Identify user and staff painpoints when processing their requests, simplify, and develop a solution.'},
+      {'name': 'Process','description': 'Curabitur fermentum finibus tellus, et efficitur augue dignissim sit amet. Cras a posuere lectus. Nulla posuere consectetur sapien, et accumsan urna condimentum a. Morbi viverra mi lorem, id bibendum neque mattis mollis. Aenean sit amet consequat nibh. Integer turpis felis, euismod in nisi nec, imperdiet faucibus magna. Morbi quam eros, accumsan quis rhoncus eget, mattis vel metus. Duis eu sapien libero. Vestibulum imperdiet rhoncus augue, in ef'},
+      {'name': 'Summary', 'description': 'Curabitur fermentum finibus tellus, et efficitur augue dignissim sit amet. Cras a posuere lectus. Nulla posuere consectetur sapien, et accumsan urna condimentum a. Morbi viverra mi lorem, id bibendum neque mattis mollis. Aenean sit amet consequat nibh. Integer turpis felis, euismod in nisi nec, imperdiet faucibus magna. Morbi quam eros, accumsan quis rhoncus eget, mattis vel metus. Duis eu sapien libero. Vestibulum imperdiet rhoncus augue, in eff'}
+  ]
 }
 
 const exampleUXRProject = {
@@ -39,7 +39,11 @@ const exampleDesignProject = {
 ]
 }
 
-let projects = Array(3).fill(exampleBothProject).concat(Array(3).fill(exampleUXRProject)).concat(Array(3).fill(exampleDesignProject))
+let projects = Array(3).fill(exampleBothProject).concat(Array(3).fill(exampleUXRProject)).concat(Array(3).fill(exampleDesignProject)).map(function(u, i) {
+  var item = JSON.parse(JSON.stringify(u)); // deep copy of object
+  item.title = u.title + " " + i // giving them a distinct title
+  return item;
+})
 
 ReactDOM.render(<App projects={projects}/>, document.getElementById('root'));
 
